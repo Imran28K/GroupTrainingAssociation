633a3d9ce2c374edb650c9054656deaba6fef151
Imran28K <imran28may@gmail.com>
2024-02-15 11:55:57 +0000

    created template
    This is a template for every newly created php/html page.

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/navfoot.css">
+</head>
+
+<body>
+    <?php include '../navfooter/navbar.php'; ?>
+     This is where you put your content in each page
+    <?php include '../navfooter/footer.php'; ?>
+</body>
+
+</html>
