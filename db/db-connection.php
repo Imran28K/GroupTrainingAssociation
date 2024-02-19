dfded362c048375a48ebd6cd883d2c8c9515e01a
Imran28K <imran28may@gmail.com>
2024-02-19 14:17:24 +0000

    did db connection

--- db/db-connection.php
+++ db/db-connection.php
@@ -1,1 +1,13 @@
-db connection
+<?php
+
+$connection = mysqli_connect("localhost:3306", "root", "", "gta_apprenticeship");
+
+if(!$connection){
+    die("Error" . mysqli_connect_error());
+}
+else
+echo 'connection established';
+
+
+
+?>
