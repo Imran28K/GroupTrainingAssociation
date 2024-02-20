d30927dd07e01377eaed179a7723a482ab62fe67
Imran28K <imran28may@gmail.com>
2024-02-20 13:04:16 +0000

    forgotpassword-query update

--- credentials/query/forgotpassword-query.php
+++ credentials/query/forgotpassword-query.php
@@ -39,6 +39,8 @@ foreach ($tableColumnMapping as $table => $emailColumn) {
 
 if ($emailFound) {
     echo "Email found in table: $tableFound";
+    header("Location: ../../send.php?email=" . urlencode($userEmail));
+    exit;
 
 } else {
     echo "Email not found in any table.";
