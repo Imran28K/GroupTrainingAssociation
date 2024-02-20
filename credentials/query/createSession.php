9e6fc01a08192f207cee34569350333d971933c1
Joe Denton <c2022171@hallam.shu.ac.uk>
2024-02-20 13:42:39 +0000

    Register pages created

--- credentials/query/createSession.php
+++ credentials/query/createSession.php
@@ -6,10 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $timeStart = $_POST['timeStart'];
     $timeEnd = $_POST['timeEnd'];
     $timeStartString = sprintf($timeStart);
-    $timeStartFinal= str_replace(':', '.', $timeStartString);
     $timeEndString = sprintf($timeEnd);
-    $timeEndFinal = str_replace(':', '.', $timeEndString);
-    echo $date;
 
     $querySession = "INSERT INTO registerSessions (SessionDate, TimeStart, TimeEnd) VALUES ('$date', '$timeStartString', '$timeEndString')";
     $resultSession = $mysqli->query($querySession);
