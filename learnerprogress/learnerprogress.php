d08ec06659c010c25b37ecc3839253463f4642ec
Imran28K <imran28may@gmail.com>
2024-02-21 14:13:08 +0000

    comments

--- learnerprogress/learnerprogress.php
+++ learnerprogress/learnerprogress.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    
-    
-    <title>Learner_Progress</title>
-   
-    <link rel="stylesheet" href="../css/learnerprogress.css" />
-  </head>
-  <body>
-    <h2 class="chart-heading">Learner Progress</h2>
-    <div class="programming-stats">
-      <div class="chart-container">
-        <canvas class="my-chart"></canvas>
-      </div>
+<?php
+//database connection to show actual results of learner progress
+//i will do the code in this file - Imran
 
-      <div class="details">
-        <ul></ul>
-      </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="main.js"></script>
-  </body>
-</html>
+?>
