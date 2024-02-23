51e749449e16c6bf3781990b5934a36fd0e674bd
jefin0703 <126799730+jefin0703@users.noreply.github.com>
2024-02-23 12:20:56 +0000

    Logout Button
    The logout button removed from the login page

--- include/navbar.php
+++ include/navbar.php
@@ -10,10 +10,8 @@
     <div class="navbar">
         
         <img src="../images/logos/gtalogo.png" alt="Logo" class="navbar-logo">
-        <a href="http://localhost/GroupTrainingAssociation/credentials/login.php">
-        <button style="background-color:indianred; padding:10px , 10px ; margin-bottom:10px; space-arero8und"> 
-            Logout 
-        </button>&nbsp;</a>
+        
+        
         <a href="index.html">Home&nbsp;&nbsp;&nbsp;&nbsp;</a>
         <a href="about.html">About&nbsp;&nbsp;&nbsp;</a>
         <a href="services.html">Services</a>
