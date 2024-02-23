eb11566fbfd8c19022f2aa0d33eb0dae42ebccb2
Imran28K <imran28may@gmail.com>
2024-02-23 15:51:03 +0000

    update forgotpassword-query.php

--- credentials/query/forgotpassword-query.php
+++ credentials/query/forgotpassword-query.php
@@ -1,36 +1,32 @@
-
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 require_once '../../db/dbconnection.php';
 $userEmail = $_POST['email'];
 
-// Define an associative array mapping tables to their email columns
+// Define an associative array mapping tables to their email and password columns
 $tableColumnMapping = [
-    'learner' => 'LearnerEmail',
-    'tutor' => 'TutorEmail',
-    'employer' => 'EmployerEmail'
+    'learner' => ['emailColumn' => 'LearnerEmail', 'passwordColumn' => 'LearnerPassword'],
+    'tutor' => ['emailColumn' => 'TutorEmail', 'passwordColumn' => 'TutorPassword'],
+    'employer' => ['emailColumn' => 'EmployerEmail', 'passwordColumn' => 'EmployerPassword']
 ];
 
 $emailFound = false;
 $tableFound = '';
 
-foreach ($tableColumnMapping as $table => $emailColumn) {
-    
-    $query = "SELECT * FROM {$table} WHERE {$emailColumn} = ?";
+foreach ($tableColumnMapping as $table => $columns) {
+    $query = "SELECT * FROM {$table} WHERE {$columns['emailColumn']} = ?";
     $stmt = $mysqli->prepare($query);
 
     if ($stmt) {
-        // Bind the user email to the query and execute
         $stmt->bind_param('s', $userEmail);
         $stmt->execute();
 
         $result = $stmt->get_result();
         if ($result->num_rows > 0) {
-            
             $emailFound = true;
             $tableFound = $table;
-            break; 
+            break;
         }
 
         $stmt->close();
@@ -40,7 +36,7 @@ foreach ($tableColumnMapping as $table => $emailColumn) {
 }
 
 if ($emailFound) {
-    $OTP = random_int(000000, 999999);
+    $OTP = random_int(100000, 999999); // Ensure the OTP is always 6 digits
     $stringOTP = strval($OTP);
     require '../../phpmailer/src/Exception.php';
     require '../../phpmailer/src/PHPMailer.php';
@@ -49,41 +45,50 @@ if ($emailFound) {
     $mail = new PHPMailer(true);
 
     try {
-        // Set mailer to use SMTP, configure settings...
+        // Mail setup...
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'imran28may@gmail.com';
-        $mail->Password = 'eahqppsbctocrcxg';
+        $mail->Username = 'imran28may@gmail.com'; // SMTP username
+        $mail->Password = 'eahqppsbctocrcxg'; // SMTP password
         $mail->SMTPSecure = 'ssl';
         $mail->Port = 465;
 
-        // Recipients
         $mail->setFrom('imran28may@gmail.com', 'GTAReset');
-        $mail->addAddress($userEmail); // Add the user's email
+        $mail->addAddress($userEmail);
 
-        // Content
         $mail->isHTML(true);
-        $mail->Subject = 'Your one time password';
+        $mail->Subject = 'Your one-time password';
         $mail->Body    = 'Your One Time Password is: ' . $stringOTP;
 
         $mail->send();
-        // Redirect to forgotpassword.php with a success message
+
+        // Update the password in the database for the found table and email
+        $passwordColumn = $tableColumnMapping[$tableFound]['passwordColumn'];
+        $updateQuery = "UPDATE {$tableFound} SET {$passwordColumn} = ? WHERE {$tableColumnMapping[$tableFound]['emailColumn']} = ?";
+        $updateStmt = $mysqli->prepare($updateQuery);
+
+        if ($updateStmt) {
+            $updateStmt->bind_param('ss', $stringOTP, $userEmail);
+            $updateStmt->execute();
+            $updateStmt->close();
+        } else {
+            echo "Error preparing update statement for table $tableFound: " . $mysqli->error;
+        }
+
         header('Location: ../forgotpassword.php?emailSent=true');
         exit();
     } catch (Exception $e) {
-        // Handle email sending failure
         header('Location: ../forgotpassword.php?emailSent=false');
         exit();
     }
-
 } else {
     header('Location: ../forgotpassword.php?emailNotFound=true');
     exit();
-    
 }
-
 ?>
+
+
 
 
 
