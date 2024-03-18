5d705ab26edc8be81196423e31660e67f0e1fa6c
Imran28K <imran28may@gmail.com>
2024-03-18 10:27:04 +0000

    update to database connection in submissions

--- users/learner/upload-submissions.php
+++ users/learner/upload-submissions.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+require_once '../../db/dbconnection.php';
+require_once 'submission-details.php';
+
+$userID = $_SESSION['userID'];
+
+// Call the function to fetch submission details and pass $userID as a parameter
+$submissions = fetchSubmissionDetails($mysqli, $userID);
+
+// The rest of your HTML code...
+?>
 <!DOCTYPE html>
 <html>
 
@@ -11,108 +23,93 @@
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
 
-<?php
-session_start();
-require_once '../../db/dbconnection.php';
-require_once 'submission-details.php';
-
-$userID = $_SESSION['userID'];
-
-$queryLearner = "SELECT * FROM learner WHERE UniqueLearnerNumber = '$userID'";
-$resultLearner = $mysqli->query($queryLearner);
-
-$obj = $resultLearner->fetch_object();
-?>
-
 <body>
 
-    <div class="wrapper">
-        <div class="sidebar">
-            <div class="profile">
-                <img src="http://localhost/GroupTrainingAssociation/images/logos/gtalogo.png" alt="profile_picture">
-                <?php
-                echo "<h3>{$obj->LearnerFirstName} {$obj->LearnerLastName}</h3>";
-                ?>
-                <p>Learner</p>
-            </div>
-            <ul>
-                <li><a href="learner.php">
-                        <span class="icon"><i class="fas fa-home"></i></span>
-                        <span class="item">View Progress</span>
-                    </a>
-                </li>
-                <li><a href="viewAttendance.php">
-                        <span class="icon"><i class="fas fa-desktop"></i></span>
-                        <span class="item">View Attendance</span>
-                    </a>
-                </li>
-                <li><a href="view-employer.php">
-                        <span class="icon"><i class="fas fa-user-friends"></i></span>
-                        <span class="item">View Employer</span>
-                    </a>
-                </li>
-                <li><a href="http://localhost/GroupTrainingAssociation/users/learner/view-apprent.php">
-                        <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
-                        <span class="item">Module Information</span>
-                    </a>
-                </li>
-                <li><a href="learnerinfo.php">
-                        <span class="icon"><i class="fas fa-user-shield"></i></span>
-                        <span class="item">User Information</span>
-                    </a>
-                </li>
-                <li><a href="upload-submissions.php" class="active">
-                        <span class="icon"><i class="fas fa-user-shield"></i></span>
-                        <span class="item">Submissions</span>
-                    </a>
-                </li>
-                <li><a href="http://localhost/GroupTrainingAssociation/credentials/login.php">
-                        <span class="icon"><i class="fas fa-door-open"></i></span>
-                        <span class="item">Logout</span>
-                    </a>
-                </li>
-            </ul>
+<div class="wrapper">
+    <div class="sidebar">
+        <div class="profile">
+            <img src="http://localhost/GroupTrainingAssociation/images/logos/gtalogo.png" alt="profile_picture">
+            <?php
+            echo "<h3>{$obj->LearnerFirstName} {$obj->LearnerLastName}</h3>";
+            ?>
+            <p>Learner</p>
         </div>
-        <div class="section">
-            <div class="top_navbar">
-                <div class="hamburger">
-                    <a href="#"><i class="fas fa-bars"></i></a>
-                </div>
-            </div>
-
-            <div class="container">
-                
-                <h2>Submission Progress</h2>
-
-                <table style="width:100%">
-                    <tr>
-                        <th>Units</th>
-                        <th>Due</th>
-                        <th>Status</th>
-                    </tr>
-                    <tr>
-                        <td>Soft Skills and Behaviour</td>
-                        <td>20/05/2026</td>
-                        <td>submitted</td>
-                    </tr>
-                    <tr>
-                        <td>English</td>
-                        <td>30/05/2026</td>
-                        <td>unsubmitted</td>
-                    </tr>
-                </table>
-            </div>
-
-        </div>
+        <ul>
+            <li><a href="learner.php">
+                    <span class="icon"><i class="fas fa-home"></i></span>
+                    <span class="item">View Progress</span>
+                </a>
+            </li>
+            <li><a href="viewAttendance.php">
+                    <span class="icon"><i class="fas fa-desktop"></i></span>
+                    <span class="item">View Attendance</span>
+                </a>
+            </li>
+            <li><a href="view-employer.php">
+                    <span class="icon"><i class="fas fa-user-friends"></i></span>
+                    <span class="item">View Employer</span>
+                </a>
+            </li>
+            <li><a href="http://localhost/GroupTrainingAssociation/users/learner/view-apprent.php">
+                    <span class="icon"><i class="fas fa-tachometer-alt"></i></span>
+                    <span class="item">Module Information</span>
+                </a>
+            </li>
+            <li><a href="learnerinfo.php">
+                    <span class="icon"><i class="fas fa-user-shield"></i></span>
+                    <span class="item">User Information</span>
+                </a>
+            </li>
+            <li><a href="upload-submissions.php" class="active">
+                    <span class="icon"><i class="fas fa-user-shield"></i></span>
+                    <span class="item">Submissions</span>
+                </a>
+            </li>
+            <li><a href="http://localhost/GroupTrainingAssociation/credentials/login.php">
+                    <span class="icon"><i class="fas fa-door-open"></i></span>
+                    <span class="item">Logout</span>
+                </a>
+            </li>
+        </ul>
     </div>
+    <div class="section">
+        <div class="top_navbar">
+            <div class="hamburger">
+                <a href="#"><i class="fas fa-bars"></i></a>
+            </div>
+        </div>
 
-    <script type="text/javascript">
-        var hamburger = document.querySelector(".hamburger");
-        hamburger.addEventListener("click", function() {
-            document.querySelector("body").classList.toggle("active");
-        })
-    </script>
+        <div class="container">
+            
+            <h2>Submission Progress</h2>
+
+            <table style="width:100%">
+                <tr>
+                    <th>Units</th>
+                    <th>Due</th>
+                    <th>Status</th>
+                </tr>
+                <?php foreach ($submissions as $submission): ?>
+                <tr>
+                    <td><?php echo $submission['unit']; ?></td>
+                    <td><?php echo $submission['due']; ?></td>
+                    <td><?php echo $submission['status']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+
+    </div>
+</div>
+
+<script type="text/javascript">
+    var hamburger = document.querySelector(".hamburger");
+    hamburger.addEventListener("click", function() {
+        document.querySelector("body").classList.toggle("active");
+    })
+</script>
 
 </body>
 
 </html>
+
