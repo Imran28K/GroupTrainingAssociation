354587ddfeab6c4682aebc93e8a73503d9f79329
Imran28K <imran28may@gmail.com>
2024-03-20 00:07:12 +0000

    Update manageSubmissions.php

--- users/tutor/manageSubmissions.php
+++ users/tutor/manageSubmissions.php
@@ -2,12 +2,13 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <title>Manage Submissions</title>
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
-  <link rel="stylesheet" type="text/css" href="../../css/learnerprogress.css">
-  <link rel="stylesheet" type="text/css" href="../../css/sidebarStyling.css">
-  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+    <meta charset="utf-8">
+    <title>Manage Submissions</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../../css/learnerprogress.css">
+    <link rel="stylesheet" type="text/css" href="../../css/sidebarStyling.css">
+    <link rel="stylesheet" type="text/css" href="../../css/tabledesign.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
 
 <?php
@@ -16,79 +17,92 @@ require_once '../../db/dbconnection.php';
 
 $userID = $_SESSION['userID'];
 
-$queryDetails = "SELECT * FROM tutor WHERE TutorID = '$userID'"; 
+$queryDetails = "SELECT * FROM tutor WHERE TutorID = '$userID'";
 $resultDetails = $mysqli->query($queryDetails);
 
-$details = $resultDetails -> fetch_object();
+$details = $resultDetails->fetch_object();
 ?>
 
 <body>
 
-  <div class="wrapper">
-    <div class="sidebar">
-      <div class="profile">
-        <img src="http://localhost/GroupTrainingAssociation/images/logos/gtalogo.png" alt="profile_picture">
-        <?php 
-        echo"<h3>{$details->TutorFirstName} {$details->TutorLastName}</h3>";
-        echo"<p>{$details->Role}</p>";
-        ?>
-      </div>
-      <ul>
-        <li><a href="tutor.php">
-            <span class="icon"><i class="fas fa-home"></i></span>
-            <span class="item">Profile Details</span>
-          </a>
-        </li>
-        <li><a href="attendanceLanding.php">
-            <span class="icon"><i class="fas fa-desktop"></i></span>
-            <span class="item">View Attendance</span>
-          </a>
-        </li>
-        <li><a href="viewLearnersTutor.php">
-            <span class="icon"><i class="fas fa-user-friends"></i></span>
-            <span class="item">View learners</span>
-          </a>
-        </li>
-        <li><a href="updateLearners.php">
-            <span class="icon"><i class="fas fa-user-friends"></i></span>
-            <span class="item">Update learners</span>
-          </a>
-        </li>
-        <li><a href="viewOTJTutor.php"> 
-            <span class="icon"><i class="fas fa-user-friends"></i></span>
-            <span class="item">Off The Job Hours</span>
-          </a>
-        </li>
-        <li><a href="manageSubmissions.php" class="active">
-            <span class="icon"><i class="fas fa-cog"></i></span>
-            <span class="item">Submissions</span>
-          </a>
-        </li>
-        <li><a href="http://localhost/GroupTrainingAssociation/credentials/login.php">
-            <span class="icon"><i class="fas fa-door-open"></i></span>
-            <span class="item">Logout</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <div class="section">
-      <div class="top_navbar">
-        <div class="hamburger">
-          <a href="#"><i class="fas fa-bars"></i></a>
+    <div class="wrapper">
+        <div class="sidebar">
+            <div class="profile">
+                <img src="http://localhost/GroupTrainingAssociation/images/logos/gtalogo.png" alt="profile_picture">
+                <?php
+                echo "<h3>{$details->TutorFirstName} {$details->TutorLastName}</h3>";
+                echo "<p>{$details->Role}</p>";
+                ?>
+            </div>
+            <ul>
+                <li><a href="tutor.php">
+                        <span class="icon"><i class="fas fa-home"></i></span>
+                        <span class="item">Profile Details</span>
+                    </a>
+                </li>
+                <li><a href="attendanceLanding.php">
+                        <span class="icon"><i class="fas fa-desktop"></i></span>
+                        <span class="item">View Attendance</span>
+                    </a>
+                </li>
+                <li><a href="viewLearnersTutor.php">
+                        <span class="icon"><i class="fas fa-user-friends"></i></span>
+                        <span class="item">View learners</span>
+                    </a>
+                </li>
+                <li><a href="updateLearners.php">
+                        <span class="icon"><i class="fas fa-user-friends"></i></span>
+                        <span class="item">Update learners</span>
+                    </a>
+                </li>
+                <li><a href="viewOTJTutor.php">
+                        <span class="icon"><i class="fas fa-user-friends"></i></span>
+                        <span class="item">Off The Job Hours</span>
+                    </a>
+                </li>
+                <li><a href="manageSubmissions.php" class="active">
+                        <span class="icon"><i class="fas fa-cog"></i></span>
+                        <span class="item">Submissions</span>
+                    </a>
+                </li>
+                <li><a href="http://localhost/GroupTrainingAssociation/credentials/login.php">
+                        <span class="icon"><i class="fas fa-door-open"></i></span>
+                        <span class="item">Logout</span>
+                    </a>
+                </li>
+            </ul>
         </div>
-      </div>
-      <div class="container">
-        
-      </div>
-    </div>
-  </div>
+        <div class="section">
+            <div class="top_navbar">
+                <div class="hamburger">
+                    <a href="#"><i class="fas fa-bars"></i></a>
+                </div>
+            </div>
+            <div class="container">
+                <h2>Submission Progress</h2>
+                <br>
 
-  <script type="text/javascript">
-    var hamburger = document.querySelector(".hamburger");
-    hamburger.addEventListener("click", function() {
-      document.querySelector("body").classList.toggle("active");
-    })
-  </script>
+                <table style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>ProgressID</th>
+                            <th>Learner</th>
+                            <th>Curent Position</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        var hamburger = document.querySelector(".hamburger");
+        hamburger.addEventListener("click", function() {
+            document.querySelector("body").classList.toggle("active");
+        })
+    </script>
 
 </body>
 
