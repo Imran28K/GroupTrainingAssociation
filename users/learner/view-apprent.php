2939bdbb5e7e93cf0f6c9e495ff9c87a1f77ae44
Adam Saffi <adamsafiullah.kashmiri@gmail.com>
2024-04-07 08:22:46 +0100

    fixed start and end date error

--- users/learner/view-apprent.php
+++ users/learner/view-apprent.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="utf-8">
   <title>Learner Info</title>
-
   <link rel="stylesheet" href="../../css/learnerinfo.css">
   <link rel="stylesheet" type="text/css" href="../../css/learnerprogress.css">
   <link rel="stylesheet" type="text/css" href="../../css/sidebarStyling.css">
@@ -24,12 +23,12 @@ $resultLearner = $mysqli->query($queryLearner);
 
 $obj = $resultLearner->fetch_object();
 
-$sql = "SELECT LearnerFirstName, LearnerLastName, LearnerEmail, Cohort, ApprenticeshipName,  EmployerID FROM learner WHERE UniqueLearnerNumber = ? ";
+$sql = "SELECT LearnerFirstName, LearnerLastName, LearnerEmail, Cohort, ApprenticeshipName, LearnerPassword, EmployerID FROM learner WHERE UniqueLearnerNumber = ? ";
 
 $stmt = mysqli_prepare($mysqli, $sql);
 mysqli_stmt_bind_param($stmt, "s", $learnerID);
 mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $LearnerFirstName, $LearnerLastName, $LearnerEmail, $Cohort, $ApprenticeshipName, $EmployerID);
+mysqli_stmt_bind_result($stmt, $LearnerFirstName, $LearnerLastName, $LearnerEmail, $Cohort, $ApprenticeshipName, $learnerPassword, $EmployerID);
 if (!mysqli_stmt_fetch($stmt)) {
   echo "0 results";
 }
@@ -85,7 +84,7 @@ mysqli_close($mysqli);
             <span class="item">View OTJ Hours</span>
           </a>
         </li>
-        <li><a href="../../users/learner/view-employer.php">
+        <li><a href="view-employer.php">
             <span class="icon"><i class="fas fa-user-friends"></i></span>
             <span class="item">View Employer</span>
           </a>
@@ -122,15 +121,23 @@ mysqli_close($mysqli);
         <div class="main">
 
           <div class="card">
-            <div class="card-body">
 
+            <?php if (isset($_GET['msg'])) { ?>
+              <div class="alert alert-success alert-dismissible mt-4 mb-2 fade show" role="alert" style="background-color:#8d599f;  margin-right:15px;margin-left:15px;">
+                <strong class=px-2 style="color:black; font-weight:Bold; margin-left:180px;">Success :</strong> <?php echo $_GET['msg']; ?>
+
+              </div>
+            <?php } ?>
+
+            <div class="card-body">
               <table class="info-table">
                 <tbody>
                   <tr>
-                    <td>Learner Name</td>
+                    <td>Name</td>
                     <td>:</td>
                     <td><?php echo $LearnerFirstName . " " . $LearnerLastName; ?> </td>
                   </tr>
+                
                   <tr>
                     <td>Apprenticeship</td>
                     <td>:</td>
@@ -162,6 +169,20 @@ mysqli_close($mysqli);
       hamburger.addEventListener("click", function() {
         document.querySelector("body").classList.toggle("active");
       })
+
+        function togglePasswordVisibility() 
+        {
+        var passwordInput = document.getElementById('password');
+        var toggleButton = document.querySelector('.toggle-password');
+        if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleButton.innerHTML = '&#128065;'; // Change to an "open eye" icon if you wish
+        } else {
+
+            passwordInput.type = 'password';
+            toggleButton.innerHTML = '&#128065;'; // Change back to the original icon
+            }
+        }
     </script>
 
 </body>
