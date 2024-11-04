4e57bc9bb1a0889161bcfa81783353bf30ea056f
Adam Saffi <adamsafiullah.kashmiri@gmail.com>
2024-11-04 16:45:57 +0000

    new connect

--- db/dbconnection.php
+++ db/dbconnection.php
@@ -2,7 +2,7 @@
     define ("DB_SERVER", "127.0.0.1") ;
     define ("DB_USERNAME", "root") ;
     define ("DB_PASSWORD", "") ;
-    define ("DB_NAME", "gta_apprenticeship") ;
+    define ("DB_NAME", "gta_apprenticeships") ;
 
     $mysqli = new mysqli (DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) ;
 
